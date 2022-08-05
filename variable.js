82584c45ba74f44c4f3f68217d74c5ebc8768d34
virali-b-patel <virali678@gmail.com>
2022-08-05 22:58:02 +0530

    add helloWorld and variable js file

---
+++ variable.js
@@ -0,0 +1,6 @@
+let a = 10;
+console.log("This is a let variable value " + a);
+var b = 20;
+console.log("This is a var variable value " + b);
+const c = 30;
+console.log("This is a const variable value " + c);
